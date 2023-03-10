222f52997ee711a3f41fb00220493ed8370c1781
Alexander Balashov <balashov187@yandex.ru>
2023-03-10 22:23:57 +0300

    2 семинар HW задача 21

--- Program.cs
+++ Program.cs
@@ -4,13 +4,32 @@
 // 782 -> 8
 // 918 -> 1
 
-Console.WriteLine("Введите трехзначное число ");
-string number = Console.ReadLine()!;
-if (number.Length == 3)
-{
-    Console.WriteLine($"{number} -> {number[1]}");
-}
-else
-{
-    Console.WriteLine("Введено неверное число");
-}
+// Console.WriteLine("Введите трехзначное число ");
+// string number = Console.ReadLine()!;
+// if (number.Length == 3)
+// {
+//     Console.WriteLine($"{number} -> {number[1]}");
+// }
+// else
+// {
+//     Console.WriteLine("Введено неверное число");
+// }
+
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+
+// 78 -> третьей цифры нет
+
+// 32679 -> 6
+
+// Console.WriteLine("Введите число ");
+// string number = Console.ReadLine()!;
+// if (number.Length < 3)
+// {
+//     Console.WriteLine("третьей цифры нет ");
+// }
+// else
+// {
+//     Console.WriteLine($"{number} -> {number[2]}");
+// }
