557b5fb8fbcf8189517a7cef5c24e825a3854721
Alexander Balashov <balashov187@yandex.ru>
2023-03-10 22:33:42 +0300

    2 семинар HW задача 3

--- Program.cs
+++ Program.cs
@@ -33,3 +33,24 @@
 // {
 //     Console.WriteLine($"{number} -> {number[2]}");
 // }
+
+// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.WriteLine("Введите число ");
+int number = int.Parse(Console.ReadLine()!);
+if (number == 6 || number == 7)
+{
+    Console.WriteLine($"{number} -> да ");
+}
+else if (number == 1 || number == 2 || number == 3 || number == 4 || number == 5)
+{
+    Console.WriteLine($"{number} -> нет ");
+}
+else
+{
+    Console.WriteLine("Введено неверное число ");
+}
